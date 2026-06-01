6dbd24fdc5d8c61f43115f34216f8b849f8b4f71
Kirill <bel-kir05@mail.ru>
2026-06-01 17:03:00 +0800

    added login page

---
+++ app/pages/login.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from '@nuxt/ui'
+import * as v from 'valibot'
+
+const schema = v.object({
+  email: v.pipe(v.string(), v.email('Invalid email')),
+  password: v.pipe(v.string(), v.minLength(8, 'Must be at least 8 characters'))
+})
+
+type Schema = v.InferOutput<typeof schema>
+
+const state = reactive({
+  email: '',
+  password: ''
+})
+
+const toast = useToast()
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  toast.add({
+    title: 'Success',
+    description: 'The form has been submitted.',
+    color: 'success'
+  })
+  console.log(event.data)
+}
+</script>
+
+<template>
+  <UForm
+    :schema="schema"
+    :state="state"
+    class="space-y-4"
+    @submit="onSubmit"
+  >
+    <UFormField
+      label="Email"
+      name="email"
+    >
+      <UInput v-model="state.email" />
+    </UFormField>
+
+    <UFormField
+      label="Password"
+      name="password"
+    >
+      <UInput
+        v-model="state.password"
+        type="password"
+      />
+    </UFormField>
+
+    <UButton type="submit">
+      Submit
+    </UButton>
+  </UForm>
+</template>
